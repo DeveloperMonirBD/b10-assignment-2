045f36492c3f785358b015f590f6bf8a9a0d3305
Monir <mrmonir0558@gmail.com>
2024-08-15 00:52:00 +0600

    Addeded javascript on hamburger button

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('ham-burger');
+const navLinks = document.getElementById('nav-links');
+
+toggleButton.addEventListener('click', () => {
+  navLinks.classList.toggle('active')
+})
